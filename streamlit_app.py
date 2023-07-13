dd5e2dea8a9a63a01b51b12fb709167dde4e8241
minmyomin <39530432+minbusiness@users.noreply.github.com>
2023-07-13 19:35:51 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,61 @@
 import streamlit as st
 import pandas as pd
 import plost
+import requests
+
+API_KEY = 'wqm4 WmNq 2snl ovAC fpvS Rmzj' # Set your API key here
+
+st.set_page_config(layout="wide")
+
+def get_token(username, password):
+response = requests.post(
+'http://project2dashboard.local/wp-json/jwt-auth/v1/token',
+data={'username': username, 'password': password},
+headers={'X-API-KEY': API_KEY}
+)
+if response.status_code == 200:
+return response.json()['token']
+else:
+return None
+
+def verify_token(token):
+response = requests.post(
+'http://project2dashboard.local/wp-json/jwt-auth/v1/token/validate',
+headers={'Authorization': f'Bearer {token}', 'X-API-KEY': API_KEY}
+)
+return response.status_code == 200
+
+def main():
+st.write("This is the main page of the application.") # Your main code goes here
+
+Check if the user is already logged in
+if 'token' in st.session_state and verify_token(st.session_state['token']):
+main() # Call the main function
+else:
+# Show the login form
+col1, col2, col3 = st.columns([1,1,1])
+with col1:
+    st.write("")
+
+with col2:
+    with st.form(key='login_form'):
+        st.title("Please log in")
+        username = st.text_input('Username')
+        password = st.text_input('Password', type='password')
+        submit_button = st.form_submit_button(label='Log in')
+
+        if submit_button:
+            token = get_token(username, password)
+            if token and verify_token(token):
+                st.session_state['token'] = token  # We store the token in the session state
+                st.experimental_rerun()  # Reload the page so that the login form disappears
+            else:
+                st.error('Access denied')
+
+with col3:
+    st.write("")
+
+
 
 st.set_page_config(layout='wide', initial_sidebar_state='expanded')
 
